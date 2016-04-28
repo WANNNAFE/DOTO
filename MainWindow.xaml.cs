8f1bb626660f1e15b84023c8420107eacb3355da
WANNNAFE <wannnafe@gmail.com>
2016-04-28 14:09:43 +0400

    Fixed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,16 +23,21 @@ namespace MetodHord
         double a, b, c;
         double f(double x)
             {
-                return Math.Pow(x,3)-0.2*Math.Pow(x,2)+0.2*x-1.2;
+                return Convert.ToDouble(TextBox11.Text)*Math.Pow(x,4)+Convert.ToDouble(TextBox12.Text)*Math.Pow(x,3)+Convert.ToDouble(TextBox13.Text)*Math.Pow(x,2)+Convert.ToDouble(TextBox14.Text)*x+Convert.ToDouble(TextBox15.Text);
             }
          double findRoot(double a, double b, double e)
             {
-                while(Math.Abs(f(b)) > e)
+                double x;
+                x = a - ((b - a) * f(a)) / (f(b) - f(a));
+                while(Math.Abs(f(x)) > e)
                 {
-                    a = b - ((b - a) * f(b))/(f(b) - f(a));
-                    b = a - ((a - b) * f(a))/(f(a) - f(b));
+                    x = a - ((b - a) * f(a)) / (f(b) - f(a));
+                    if ((f(x) * f(a)) < 0) x = b; 
+                    if (f(x)*f(b)<0) a = x;
+                  //  a = b - ((b - a) * f(b))/(f(b) - f(a));
+                  //  b = a - ((a - b) * f(a))/(f(a) - f(b));
                 }
-                return b;
+                return x;
             }
 
         public MainWindow()
@@ -49,8 +54,7 @@ namespace MetodHord
             a = Convert.ToDouble(TextBox1.Text);
             b = Convert.ToDouble(TextBox2.Text);
             c = Convert.ToDouble(TextBox3.Text);
-            TextBlock1.Text = Convert.ToString(Convert.ToInt64(findRoot(a, b, c) / c) * c) + " " + Convert.ToString(a) + " " + Convert.ToString(b);
-
+            TextBlock1.Text = Convert.ToString(Convert.ToInt64(findRoot(a, b, c) / c) * c);// + " " + Convert.ToString(a) + " " + Convert.ToString(b);
         }
     }
 }
